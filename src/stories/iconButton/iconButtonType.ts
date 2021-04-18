72bdd84abc92729534b5b82031465071825c8f9a
takeshisakuma <saude0518@gmail.com>
2021-04-18 13:48:41 +0900

    fix iconButtonType

--- src/stories/iconButton/iconButtonType.ts
+++ src/stories/iconButton/iconButtonType.ts
@@ -1,7 +1,7 @@
 export type iconButtonType = {
   roll?: "normal" | "danger" | "submit";
   size?: "small" | "medium" | "large";
-  label: "any";
+  label: any;
   abled?: boolean;
   img: "search" | "externallink" | "internallink" | "none";
   onClick?: () => void;
